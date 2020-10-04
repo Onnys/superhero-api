11e973a8e3ca8ba023ef422216a76f1f3cc671b1
onnys <onnysanild@gmail.com>
2020-10-04 12:01:55 +0200

    first commit

---
+++ Models.py
@@ -0,0 +1,116 @@
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+
+database_name = 'superhero'
+database_path = "postgres://{}:{}@{}/{}".format('superhero','hero','localhost:5432', database_name)
+db = SQLAlchemy()
+
+def setup_db(app, database_path= database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class superheros(db.Model):
+    __tablename__ = 'superheros'
+    id = Column (Integer, primary_key=True)
+    name = Column(String)
+    slug = Column(String)
+    #powerstats
+    intelligence = Column(Integer)
+    strength = Column(Integer)
+    speed = Column(Integer)
+    durability = Column(Integer)
+    power = Column(Integer)
+    combat = Column(Integer)
+    # appearance
+    gender = Column(String)
+    race = Column(String)
+    height = Column(db.ARRAY(String))
+    weight = Column(db.ARRAY(String))
+    eyeColor = Column(String)
+    hairColor = Column(String)
+    # biography
+    fullName = Column(String)
+    alterEgos = Column(String)
+    aliases = Column(db.ARRAY(String))
+    placeOfBirth = Column(String)
+    firstAppearance = Column(String)
+    publisher = Column(String)
+    alignment = Column(String)
+    # work
+    occupation = Column(String)
+    base = Column(String)
+    # connections
+    groupAffiliation = Column(String)
+    relatives = Column(String)
+    # images
+    xs = Column(String)
+    sm = Column(String)
+    md = Column(String)
+    lg = Column(String)
+
+    def __init__(self, name, slug, intelligence, strength, speed, durability,power, combat, gender,race, height, weight, eyeColor, hairColor, fullName, alterEgos, aliases, placeOfBirth, firstAppearance, publisher, alignment, occupation, base, groupAffiliation, relatives, xs, sm, md,lg):
+        self.name = name
+        self.slug = slug
+        self.intelligence = intelligence
+        self.strength = strength
+        self.speed = speed
+        self.durability = durability
+        self.power = power
+        self.combat = combat
+        self.gender = gender
+        self.race = race
+        self.height = height
+        self.weight = weight
+        self.eyeColor = eyeColor
+        self.hairColor = hairColor
+        self.fullName = fullName
+        self.alterEgos = alterEgos
+        self.aliases = aliases
+        self.placeOfBirth = placeOfBirth
+        self.firstAppearance = firstAppearance
+        self.publisher = publisher
+        self.alignment = alignment
+        self.occupation = occupation
+        self.base = base
+        self.groupAffiliation = groupAffiliation
+        self.relatives = relatives
+        self.xs = xs
+        self.sm = sm
+        self.md = md
+        self.lg = lg
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def format(self):
+         return {
+             'id': self.id,
+             'name': self.name,
+             'slug': self.slug,
+             'powerstats': {
+                 "intelligence": self.intelligence,
+                 "strength": self.strength,
+                 "speed": self.speed,
+                 "durability": self.durability,
+                 "power": self.power,
+                 "combat": self.combat
+             }
+         }
+    # def powerstats(self):
+    #     return {
+
+    #     }
---
+++ app.py
@@ -0,0 +1,115 @@
+from flask import Flask, request, jsonify, abort
+from flask_cors import CORS
+from Models import setup_db , superheros
+import json
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,POST,DELETE')
+        return response
+
+    @app.route("/")
+    def hello():
+        # hero = Hero(name='onnys', slug='ola')
+    
+        return jsonify({
+            'success': True
+        })
+
+    @app.route('/superhero-api', methods=['GET'])
+    def superhero_api():
+        superhero = superheros.query.all()
+        formated_hero = [superhero.format() for superheros in superhero]
+        return jsonify({
+        #   'name': hero.name,
+        #   'slug': hero.slug,
+          'powerstats': formated_hero, 
+        #   'appearance':,
+        #   'biography':,
+        #   'work':,
+        #   'connections':
+        #   'images':
+        })
+
+    @app.route('/add-superhero', methods=['POST'])
+    def add_superhero():
+        body = request.get_json()
+        new_name = body.get('name', None)
+        new_slug = body.get('slug',None)
+        powerstats = body.get('powerstats',None)
+        new_intelligence = powerstats['intelligence']
+        new_strength = powerstats['strength']
+        new_speed = powerstats['speed']
+        new_durability = powerstats['durability']
+        new_power = powerstats['power']
+        new_combat = powerstats['combat']
+        # appearance
+        appearance = body.get('appearance',None)
+        new_gender = appearance['gender']
+        new_race = appearance['race']
+        new_height = appearance['height']
+        new_weight = appearance['weight']
+        new_eyeColor = appearance['eyeColor']
+        new_hairColor = appearance['hairColor']
+        # biography
+        biography = body.get('biography',None)
+        new_fullName = biography['fullName']
+        new_alterEgos = biography['alterEgos']
+        new_aliases = biography['aliases']
+        new_placeOfBirth = biography['placeOfBirth']
+        new_firstAppearance = biography['firstAppearance']
+        new_publisher = biography['publisher']
+        new_alignment = biography['alignment']
+        # work
+        work = body.get('work',None)
+        new_occupation = work['occupation']
+        new_base = work['base']
+        # connections
+        connections = body.get('connections',None)
+        new_groupAffiliation = connections['groupAffiliation']
+        new_relatives = connections['relatives']
+        # images
+        images = body.get('images',None)
+        new_xs = images['xs']
+        new_sm = images['sm']
+        new_md = images['md']
+        new_lg = images['lg']
+        try:
+            superhero = superheros(name = new_name, slug = new_slug, intelligence = new_intelligence,
+                    strength = new_strength, speed = new_speed, durability = new_durability, power = new_power,
+                    combat = new_combat, gender = new_gender, race = new_race, height = new_height, weight = new_weight,
+                    eyeColor = new_eyeColor, hairColor = new_hairColor, fullName = new_fullName, alterEgos = new_alterEgos,
+                    aliases = new_aliases, placeOfBirth = new_placeOfBirth, firstAppearance = new_firstAppearance, publisher = new_publisher,
+                    alignment = new_alignment, occupation = new_occupation, base = new_base, groupAffiliation = new_groupAffiliation,
+                    relatives = new_relatives, xs = new_xs, sm = new_sm, md = new_md, lg= new_lg)
+            superhero.insert()   
+        except:
+            abort(400)
+        
+        return jsonify({
+            'success': True
+        })
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'bad request'
+        }), 400
+
+    return app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
